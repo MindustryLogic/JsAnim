5394e15a4d1184213b5ffa519d600ce165be45b8
MindustryLogic <xyndermultilanguagelism@gmail.com>
2025-03-10 18:47:42 +0900

    svg shows up yay

--- JsAnim.js
+++ JsAnim.js
@@ -23,17 +23,20 @@ export class Main{
         this.id = name;
         this.fps;
         this.AppendTarget;
+        this.reso;
     }
     initDisplay(resolution, fps) {
         let ResolutionWidth = Number(resolution.split("x")[0]);
         let ResolutionHeight = Number(resolution.split("x")[1]);
         let MsPerFrame = Math.round(1000 / fps);
+        this.reso = resolution;
         this.fps = fps;
         const frame = document.createElement("div");
         frame.id = this.id;
-        frame.style.backgroundColor = "black";
+        frame.style.backgroundColor = "rgba(0, 0, 0, 0)";
         frame.style.height = ResolutionHeight + "px";
         frame.style.width = ResolutionWidth + "px";
+        frame.style.borderStyle = "solid";
         document.body.appendChild(frame);
     }
     DisplayData() {
@@ -48,21 +51,36 @@ export class Main{
         switch (this.object) {
             case "rect":
                 
-        }
-    }
-    CreateRectCSS(width, height, bgColor) {
-        const rect = createElement("div");
+        };
+    };
+    CreateRectCSS(width, height, Color) {
+        const rect = document.createElement("div");
         rect.style.width = width + "px";
         rect.style.height = height + "px";
         rect.style.position = "relative";
-        rect.style.backgroundColor = bgColor;
+        rect.style.backgroundColor = Color;
         this.AppendTarget.appendChild(rect);
-    }
-    CreateRectSVG(width, height, startPos) {
-        const svg = document.createElementNS("https://www.w3.org/2000/svg", "svg")
-        const rect = document.createElementNS("https://www.w3.org/2000/svg", "rect")
-        svg.setAttribute("viewBox",)
-    }
+    };
+    CreateRectSVG(width, height, startPos, color, id) {
+        var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        var rect1 = document.createElementNS("http://www.w3.org/2000/svg", 'rect');
+            
+        svg.setAttribute("aria-hidden","true");
+        svg.setAttribute("viewbox", "0 0" + " " + this.reso.split("x")[0] + " " + this.reso.split("x")[1]);
+        svg.setAttribute('width', this.reso.split("x")[0]+"px");
+        svg.setAttribute('height', this.reso.split("x")[1]+"px");
+        svg.setAttribute('id', id);
+        svg.style.position = "relative";
+            
+        rect1.setAttribute("width", width+"px");
+        rect1.setAttribute("height", height+"px");
+        rect1.setAttribute("x", startPos.split("x")[0]);
+        rect1.setAttribute("y", startPos.split("x")[1]);
+        rect1.setAttribute("fill", color);
+
+        svg.appendChild(rect1);
+        this.AppendTarget.appendChild(svg);
+    };
 }
 
 export const Anim = new Main("RenderZone");
--- UsedInShow.js
+++ UsedInShow.js
@@ -2,4 +2,6 @@ import * as ManimJs from './JsAnim.js';
 
 ManimJs.Anim.initDisplay("1080x720", 30);
 ManimJs.Anim.DisplayData();
+ManimJs.Anim.CreateRectCSS(20, 50, "blue");
+ManimJs.Anim.CreateRectSVG(50, 60, "100x80", "red", "hi");
 
